601e51f9b5ae49acee4567c12b6d6cece6589f0a
Baek-sohyeon <55051191+Baek-sohyeon@users.noreply.github.com>
2021-07-21 22:46:14 +0900

    Add Title

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import {Btn} from './components/Btn'
+import {Title} from './components/Title';
 
 function App() {
   return (
       <>
-        <Btn name="꼰대 유형 보기 >"/>
+        {/*<Btn name="꼰대 유형 보기 >"/>*/}
+        <Title/>
       </>
   );
 }
