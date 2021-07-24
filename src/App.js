de5d41cb38870621564fe07bf8895428bf57e207
Baek-sohyeon <55051191+Baek-sohyeon@users.noreply.github.com>
2021-07-24 17:29:32 +0900

    Merge All

--- src/App.js
+++ src/App.js
@@ -1,29 +1,13 @@
-import React, {useState} from "react";
-import {Btn} from './components/Btn'
+import React from "react";
 import {Title} from './components/Title';
-import {TypeModal} from './components/TypeModal'
 import {MainDB} from './components/MainDB'
 
 function App() {
-
-    // const [open, setOpen] = useState(false);
-    //
-    // const clicked=()=>{
-    //     setOpen(true)
-    // }
-    //
-    // const close=()=>{
-    //     setOpen(false)
-    // }
 
     return (
       <div>
         <Title/>
         <MainDB/>
-        {/*<Btn*/}
-        {/*    clicked={clicked}*/}
-        {/*/>*/}
-        {/*  {open && <TypeModal close={close}/>}*/}
       </div>
     )
 
