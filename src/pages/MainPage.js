65c2cfbc5055321e16b2fdcb5a5591644758444d
Baek-sohyeon <55051191+Baek-sohyeon@users.noreply.github.com>
2021-07-25 14:19:29 +0900

    Merge All

--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -1,12 +1,14 @@
 import React from "react";
 import {Title} from '../components/Title';
 import {MainDB} from '../components/MainDB'
+// import { BrowserRouter as Router, Route, Switch, NavLink } from 'react-router-dom';
 
 export function MainPage() {
 
     return <>
         <Title/>
         <MainDB/>
+        {/*<button><NavLink to="/Test">Test</NavLink></button>*/}
     </>
 
 }
