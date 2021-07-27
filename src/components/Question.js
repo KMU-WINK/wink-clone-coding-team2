1f8eafcef3b6a2aad0d4728723afe7aa0a12acfc
송민준 <minjun7410@gmail.com>
2021-07-28 00:46:01 +0900

    add QuestionBox

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,4 +1,21 @@
 import React from "react"
 import "./Question.css"
+import {QuestionList} from "./QuestionList"
 
-export function Question()
+export function Question({quest}){
+    return(
+        <div className="Question">
+            <strong className="text">{quest.index}. {quest.quest.question}</strong>
+        </div>
+    )
+}
+
+export function QuestionBox(){
+    return(
+        <div className="QuestionBox">
+            {QuestionList.map((quest, index) => (
+                <Question index={index+1} quest={quest}/>
+            ))}
+        </div>
+    )
+}
--- src/pages/TestPage.js
+++ src/pages/TestPage.js
@@ -1,10 +1,12 @@
 import React from "react";
 import {TestTitle} from "../components/TestTitle";
+import {Question} from "../components/Question";
 
 export function TestPage() {
 
     return <>
         <TestTitle/>
+        <Question/>
     </>
 
 }
