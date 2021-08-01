7c74fc15a46c7e7eccfc5398856106ca53af609b
SangwonYoon <yoonsw0532@naver.com>
2021-08-01 14:49:31 +0900

    Footer

--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -1,7 +1,7 @@
 import React from "react";
 import {Title} from '../components/Title';
 import {MainDB} from '../components/MainDB';
-import {MainDB} from '../components/Footer';
+import {Footer} from '../components/Footer';
 
 export function MainPage() {
 
