3886786b09e3b488d1a5dedcee05d742fd85719c
SangwonYoon <yoonsw0532@naver.com>
2021-08-01 15:28:14 +0900

    TestControl

--- src/pages/TestPage.js
+++ src/pages/TestPage.js
@@ -1,10 +1,12 @@
 import React from "react";
+import { TestControl } from "../components/TestControl";
 import {TestTitle} from "../components/TestTitle";
 
 export function TestPage() {
 
     return <>
         <TestTitle/>
+        <TestControl page="1"/>
     </>
 
 }
