e178c3c980b29358b1abe0cbb1d3a4d82c2e5940
송민준 <minjun7410@gmail.com>
2021-08-02 17:32:37 +0900

    Add useState

--- src/components/Question/Answer.js
+++ src/components/Question/Answer.js
@@ -1,10 +1,14 @@
 import React from "react"
 import "./Answer.css"
 
-export function Answer({bold, answer, order}){
+export function Answer({bold, answer, order, select, Radio, sendRadio}){
+    const radioChange = (event) => {
+        console.log(event.target.value);
+        sendRadio(event.target.value);
+    }
     return(
         <label className="Answer">
-            <input type="radio" name={order}></input>
+            <input type="radio" name={order} value={select} onChange={(e) => radioChange(e)}></input>
             <div className="RadioCircle">
                 <div></div>
             </div>
